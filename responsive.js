2ea57ca02ee26de7fbfccddabf65cb1b3e3ea08b
ajay bhatt <ajaybhattit@gmail.com>
2023-08-06 19:57:06 +0700

    issue solved

--- responsive.js
+++ responsive.js
@@ -1,9 +1,11 @@
 let element = document.querySelector("i");
 let overlay = document.querySelector("#overlay");
+let nav = document.querySelector("#big-outer-circle");
 
 element.onclick = () => {
-    element.style.display = "hidden";
+    element.style.display = "none";
     overlay.style.display = "initial";
+    nav.style.display = "none";
 
 }
 
@@ -11,6 +13,8 @@ let close = document.querySelector("#close-button");
 close.onclick = () => {
     overlay.style.display = "none";
     element.style.display = "initial";
+    nav.style.display = "initial";
+
 
 
 }
